35bb6d3f44f04fadbb9e3ea36a71b26c99fe982e
yashwanth-tfs <yashwanth.venampalli@thermofisher.com>
2026-01-22 13:44:32 +0530

    removing unwanted folders

--- generated-projects/test-python-api/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SampleModel
-
-class SampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SampleModel
-        fields = '__all__'
--- generated-projects/test-python-api/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import SampleModel
-from .serializers import SampleSerializer
-
-class SampleViewSet(viewsets.ModelViewSet):
-    queryset = SampleModel.objects.all()
-    serializer_class = SampleSerializer
